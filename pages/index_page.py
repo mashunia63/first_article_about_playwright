cc92ca372a1fec8ac0d60c21a35d24402570dcf5
mashunia63 <mashunia63@mail.ru>
2023-08-09 16:23:18 +0300

    done

--- pages/index_page.py
+++ pages/index_page.py
@@ -3,6 +3,15 @@ import config
 
 
 class IndexPage:
+    _BUTTON_GOOGLE_SEARCH = "body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > input.gNO89b"
+    _LINK_ENGLISH_LANG = "//a[contains(text(), 'English')]"
 
     def open_index_page(self, page: Page) -> None:
         page.goto(config.url.DOMAIN)
+
+    def press_link_english_lang(self, page: Page):
+        page.locator(self._LINK_ENGLISH_LANG).click()
+
+    def get_text_from_google_search_button (self,page:Page) -> None:
+        return page.locator(self._BUTTON_GOOGLE_SEARCH).get_attribute('value')
+
